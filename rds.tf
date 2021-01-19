c649c6e63ca6d0d4d7eb2818a6e5d4016ec11363
lbugaian <leonidbugaian@gmail.com>
2021-01-19 07:43:56 -0600

    changing the code

--- rds.tf
+++ rds.tf
@@ -1,12 +1,8 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = file("../rds_pass.txt")
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+resource "aws_instance" "web" {
+  ami           = "ami-0be2609ba883822ec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "kv-terraform"
+  }
 }
